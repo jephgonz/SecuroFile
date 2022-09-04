59bb88d3a25d74743b54e1b4cdd0c4068d42d55f
jephgonz <72686095+jephgonz@users.noreply.github.com>
2022-09-04 17:56:21 +0800

    patch

--- File Encryption/main.py
+++ File Encryption/main.py
@@ -3,7 +3,6 @@ import base64
 import bcrypt
 import hashlib
 import tkinter as tk
-from tkinter import ttk
 from tkinter import *
 from tkinter import filedialog
 from Crypto.Cipher import AES
@@ -14,35 +13,25 @@ import mysql.connector
 import zipfile
 from pathlib import Path
 
-# DB CONNECTION
-con = mysql.connector.connect(host="localhost", user="root", password="", database="capstone")
-cursor = con.cursor()
-
-# TEST GET USERS WITH DEVICES
-query = "select * from user_devices"
-cursor.execute(query)
-table = cursor.fetchall()
-for row in table:
-    print(row[0])
-    print(row[1])
-    print(row[2])
-    print(row[3])
-    print(row[4])
-print("TEST GET USERS WITH DEVICES DONE.")
-
-# VARIABLES
+# GLOBAL VARIABLES
 BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
 list_files = ['cache/header', 'cache/key', 'cache/enc']
+
 key = 'placeholder'
+
 user_id = '1'  # ADD USER_ID AFTER LOGIN
+
 current_machine_id = str(subprocess.check_output('wmic csproduct get uuid'), 'utf-8').split('\n')[1].strip()
 print(current_machine_id)
 print("TEST GET CURRENT MACHINE ID DONE.")
 
+FONT = ('Nirmala UI', 16, 'bold')
 
-# --------------------- INIT ---------------------
+
+# TKINTER INIT
 class tkinterApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -65,37 +54,38 @@ class tkinterApp(tk.Tk):
         frame.tkraise()
 
 
-# -------------------- LOGIN ---------------------
+# LOGIN
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.configure(bg="#fff")
-        frame = Frame(self, width=350, height=550, bg="red")
+        self.configure(bg="#292F36")
+        frame = Frame(self, width=350, height=550, bg="#292F36")
         frame.place(x=25, y=25)
 
-        heading = Label(frame, text='Login', fg="#000", bg="white", font=('Arial', 24, 'bold'))
-        heading.grid(row=1)
+        heading = Label(frame, text='LOGIN', fg="#FFFFFF", bg="#292F36", font=FONT)
+        heading.grid(row=1, columnspan=2)
 
         EMAIL = StringVar()
         PASS = StringVar()
 
-        lbl_username = Label(frame, text="Email:", font=('arial', 18), bd=18, bg="#fff")
+        lbl_username = Label(frame, text="Email:", font=FONT, bd=10, fg="#FFFFFF", bg="#292F36")
         lbl_username.grid(row=2)
-        lbl_password = Label(frame, text="Password:", font=('arial', 18), bd=18, bg="#fff")
+        lbl_password = Label(frame, text="Password:", font=FONT, bd=10, fg="#FFFFFF", bg="#292F36")
         lbl_password.grid(row=3)
 
-        email = Entry(frame, font=('arial', 20), textvariable=EMAIL, width=15)
+        email = Entry(frame, font=FONT, textvariable=EMAIL, width=19)
         email.grid(row=2, column=1)
-        pass1 = Entry(frame, font=('arial', 20), textvariable=PASS, width=15, show="*")
+        pass1 = Entry(frame, font=FONT, textvariable=PASS, width=19, show="*")
         pass1.grid(row=3, column=1)
-        lbl_result = Label(frame, text="", font=('arial', 18), bg="#fff")
+        lbl_result = Label(frame, text="", font=FONT, fg="#FFFFFF", bg="#292F36")
         lbl_result.grid(row=4, columnspan=2)
 
-        btn_login = Button(frame, font=('arial', 20), text="Login",
-                           command=lambda: login_user(EMAIL.get(), PASS.get(), lbl_result))
+        btn_login = Button(frame, font=FONT, text="Login",
+                           command=lambda: login_user(EMAIL.get(), PASS.get(), lbl_result), fg="#FFFFFF", bg="#4ECDC4")
         btn_login.grid(row=5, columnspan=2)
 
-        button2 = Button(frame, font=('arial', 20), text="Register", command=lambda: controller.show_frame(Page1))
+        button2 = Button(frame, font=FONT, text="Register", command=lambda: controller.show_frame(Page1), fg="#FFFFFF",
+                         bg="#FF6B6B")
         button2.grid(row=6, columnspan=2)
 
         def database():
@@ -129,17 +119,15 @@ class StartPage(tk.Frame):
                     lbl_result.config(text="Email not registered", fg="red")
 
 
-# ------------------- REGISTER -------------------
+# REGISTER
 class Page1(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.configure(bg="#fff")
-        frame = Frame(self, width=350, height=550, bg="red")
+        self.configure(bg="#292F36")
+        frame = Frame(self, width=350, height=550, bg="#292F36")
         frame.place(x=25, y=25)
-        # img = PhotoImage(file='media/user.png')
-        # Label(frame, image=img, bg='white').place(x=0, y=0)
-        heading = Label(frame, text='Register', fg="#000", bg="white", font=('Arial', 24, 'bold'))
-        heading.grid(row=1)
+        heading = Label(frame, text='REGISTER', fg="#FFFFFF", bg="#292F36", font=FONT)
+        heading.grid(row=1, columnspan=2)
 
         FNAME = StringVar()
         MNAME = StringVar()
@@ -148,40 +136,42 @@ class Page1(tk.Frame):
         PASS = StringVar()
         RPASS = StringVar()
 
-        lbl_firstname = Label(frame, text="First name:", font=('arial', 18), bd=18)
+        lbl_firstname = Label(frame, text="First name:", font=FONT, bd=5, fg="#FFFFFF", bg="#292F36")
         lbl_firstname.grid(row=2)
-        lbl_firstname = Label(frame, text="Middle name:", font=('arial', 18), bd=18)
+        lbl_firstname = Label(frame, text="Middle name:", font=FONT, bd=5, fg="#FFFFFF", bg="#292F36")
         lbl_firstname.grid(row=3)
-        lbl_lastname = Label(frame, text="Last name:", font=('arial', 18), bd=18)
+        lbl_lastname = Label(frame, text="Last name:", font=FONT, bd=5, fg="#FFFFFF", bg="#292F36")
         lbl_lastname.grid(row=4)
-        lbl_username = Label(frame, text="Email:", font=('arial', 18), bd=18)
+        lbl_username = Label(frame, text="Email:", font=FONT, bd=5, fg="#FFFFFF", bg="#292F36")
         lbl_username.grid(row=5)
-        lbl_password = Label(frame, text="Password:", font=('arial', 18), bd=18)
+        lbl_password = Label(frame, text="Password:", font=FONT, bd=5, fg="#FFFFFF", bg="#292F36")
         lbl_password.grid(row=6)
-        lbl_password = Label(frame, text="Repeat password:", font=('arial', 18), bd=18)
+        lbl_password = Label(frame, text="Repeat password:", font=FONT, bd=5, fg="#FFFFFF", bg="#292F36")
         lbl_password.grid(row=7)
-        lbl_result = Label(frame, text="", font=('arial', 18))
+        lbl_result = Label(frame, text="", font=FONT, fg="#FFFFFF", bg="#292F36")
         lbl_result.grid(row=8, columnspan=2)
 
-        fname = Entry(frame, font=('arial', 20), textvariable=FNAME, width=15)
+        fname = Entry(frame, font=FONT, textvariable=FNAME, width=14)
         fname.grid(row=2, column=1)
-        mname = Entry(frame, font=('arial', 20), textvariable=MNAME, width=15)
+        mname = Entry(frame, font=FONT, textvariable=MNAME, width=14)
         mname.grid(row=3, column=1)
-        lname = Entry(frame, font=('arial', 20), textvariable=LNAME, width=15)
+        lname = Entry(frame, font=FONT, textvariable=LNAME, width=14)
         lname.grid(row=4, column=1)
-        email = Entry(frame, font=('arial', 20), textvariable=EMAIL, width=15)
+        email = Entry(frame, font=FONT, textvariable=EMAIL, width=14)
         email.grid(row=5, column=1)
-        pass1 = Entry(frame, font=('arial', 20), textvariable=PASS, width=15, show="*")
+        pass1 = Entry(frame, font=FONT, textvariable=PASS, width=14, show="*")
         pass1.grid(row=6, column=1)
-        rpass1 = Entry(frame, font=('arial', 20), textvariable=RPASS, width=15, show="*")
+        rpass1 = Entry(frame, font=FONT, textvariable=RPASS, width=14, show="*")
         rpass1.grid(row=7, column=1)
 
-        btn_register = Button(frame, font=('arial', 20), text="Register", state=NORMAL,
+        btn_register = Button(frame, font=FONT, text="Register", state=NORMAL,
                               command=lambda: register_user(FNAME.get(), MNAME.get(), LNAME.get(), EMAIL.get(),
-                                                            PASS.get(), RPASS.get(), lbl_result, btn_register))
+                                                            PASS.get(), RPASS.get(), lbl_result, btn_register),
+                              fg="#FFFFFF", bg="#4ECDC4")
         btn_register.grid(row=9, columnspan=2)
 
-        button2 = Button(frame, font=('arial', 20), text="Login", command=lambda: controller.show_frame(StartPage))
+        button2 = Button(frame, font=FONT, text="Login", command=lambda: controller.show_frame(StartPage), fg="#FFFFFF",
+                         bg="#FF6B6B")
         button2.grid(row=10, columnspan=2)
 
         def database():
@@ -213,78 +203,11 @@ class Page1(tk.Frame):
                     lbl_result.config(text="Password does not match!", fg="red")
 
 
-# ------------------- MAIN APP -------------------
-class Page2(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page 2", font=('arial', 20))
-        label.grid(row=0, column=4, padx=10, pady=10)
-
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text="Page 1",
-                             command=lambda: controller.show_frame(Page1))
-
-        # putting the button in its place by
-        # using grid
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        # button to show frame 3 with text
-        # layout3
-        button2 = ttk.Button(self, text="Startpage",
-                             command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=2, column=1, padx=10, pady=10)
-
-
-# ----------------- DRIVER CODE ------------------
-app = tkinterApp()
-app.mainloop()
-
-
-# -------------- START OF FUNCTIONS --------------
+# MAIN APP
 def database():
     global con, cursor
     con = mysql.connector.connect(host="localhost", user="root", password="", database="capstone")
     cursor = con.cursor()
-
-
-def encrypt(raw, key):
-    private_key = hashlib.sha256(key.encode('utf8')).digest()
-    BS = AES.block_size
-    pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-    raw = base64.b64encode(pad(raw).encode('utf8'))
-    iv = get_random_bytes(AES.block_size)
-    cipher = AES.new(key=private_key, mode=AES.MODE_CFB, iv=iv)
-    return base64.b64encode(iv + cipher.encrypt(raw))
-
-
-def decrypt(enc, key):
-    private_key = hashlib.sha256(key.encode('utf8')).digest()
-    unpad = lambda s: s[:-ord(s[-1:])]
-    enc = base64.b64decode(enc)
-    iv = enc[:AES.block_size]
-    cipher = AES.new(private_key, AES.MODE_CFB, iv)
-    return unpad(base64.b64decode(cipher.decrypt(enc[AES.block_size:])).decode('utf8'))
-
-
-def writeenc(encrypted):
-    file = open('cache/enc', "wb")
-    file.write(encrypted)
-    file.close()
-
-
-def compressenc(file_name):
-    with zipfile.ZipFile('encrypted/' + file_name + '.enc', 'w') as zipF:
-        for file in list_files:
-            zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
-
-
-def extractenc(file_name):
-    with zipfile.ZipFile('encrypted/' + file_name + '.enc', 'r') as zip_ref:
-        zip_ref.extractall('')
 
 
 def genkey():
@@ -325,36 +248,78 @@ def updateDevStat(devId, status):
     con.commit()
 
 
-# --------------- END OF FUNCTIONS ---------------
+class Page2(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="#292F36")
+        frame = Frame(self, width=350, height=550, bg="#292F36")
+        frame.place(x=25, y=25)
 
-# ----------------- INITIATIONS ------------------
-genkey()
-regdev(current_machine_id)
-updateDevStat('8', 'Inactive')
+        btn_encrypt = Button(frame, font=FONT, text="ENCRYPT", state=NORMAL, command=lambda: (encrypt_file()),
+                             fg="#FFFFFF", bg="#4ECDC4")
+        btn_encrypt.grid(row=1, columnspan=2)
+        btn_decrypt = Button(frame, font=FONT, text="ENCRYPT", state=NORMAL, command=lambda: (decrypt_file()),
+                             fg="#FFFFFF", bg="#FF6B6B")
+        btn_decrypt.grid(row=1, columnspan=2)
 
-# ------------------ ENCRYPTION ------------------
-root = tk.Tk()
-root.withdraw()
-file = filedialog.askopenfilename()
-file_name = Path(file).stem
-file = open(file, "rb")
-raw = str(file.read())
-encrypted = encrypt(raw, key)
-print(encrypted)
-file.close()
-writeenc(encrypted)
+        genkey()
+        regdev(current_machine_id)
+        updateDevStat('8', 'Inactive')
 
-# Compile File
-compressenc(file_name)
+        def encrypt_file():
+            root = tk.Tk()
+            root.withdraw()
+            file = filedialog.askopenfilename()
+            file_name = Path(file).stem
+            file = open(file, "rb")
+            raw = str(file.read())
+            encrypted = encrypt(raw, key)
+            print(encrypted)
+            file.close()
+            writeenc(encrypted)
+            compressenc(file_name)
 
-# ------------------ DECRYPTION ------------------
-root = tk.Tk()
-root.withdraw()
-file = filedialog.askopenfilename()
-file_name = Path(file).stem
+        def encrypt(raw, key):
+            private_key = hashlib.sha256(key.encode('utf8')).digest()
+            BS = AES.block_size
+            pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+            raw = base64.b64encode(pad(raw).encode('utf8'))
+            iv = get_random_bytes(AES.block_size)
+            cipher = AES.new(key=private_key, mode=AES.MODE_CFB, iv=iv)
+            return base64.b64encode(iv + cipher.encrypt(raw))
 
-# Decompress Encrypted File
-extractenc(file_name)
+        def writeenc(encrypted):
+            file = open('cache/enc', "wb")
+            file.write(encrypted)
+            file.close()
 
-decrypted = decrypt(encrypted, key)
-print(decrypted)
+        def compressenc(file_name):
+            with zipfile.ZipFile('encrypted/' + file_name + '.enc', 'w') as zipF:
+                for file in list_files:
+                    zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
+
+        def decrypt_file():
+            root = tk.Tk()
+            root.withdraw()
+            file = filedialog.askopenfilename()
+            file_name = Path(file).stem
+            extractenc(file_name)
+            decrypted = decrypt(encrypted, key)
+            print(decrypted)
+
+        def decrypt(enc, key):
+            private_key = hashlib.sha256(key.encode('utf8')).digest()
+            unpad = lambda s: s[:-ord(s[-1:])]
+            enc = base64.b64decode(enc)
+            iv = enc[:AES.block_size]
+            cipher = AES.new(private_key, AES.MODE_CFB, iv)
+            return unpad(base64.b64decode(cipher.decrypt(enc[AES.block_size:])).decode('utf8'))
+
+        def extractenc(file_name):
+            with zipfile.ZipFile('encrypted/' + file_name + '.enc', 'r') as zip_ref:
+                zip_ref.extractall('')
+
+
+# DRIVER CODE
+app = tkinterApp()
+app.mainloop()
